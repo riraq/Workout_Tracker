aec437b988aa4e53e438e3286cbf4912f829c716
riraq <riraq@mail.usf.edu>
2021-04-28 15:16:19 -0500

    fixed models; updated routes

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -2,10 +2,17 @@ const router = require('express').Router();
 
 const db = require('../../models')
 
+router.get('/workouts', async (req, res) => {
+  try {
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/workouts', async (req, res) => {
   try {
     console.log("res: ", res)
-
     res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
