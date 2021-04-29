e86f1626484d8be30fe0d8f15377dfb3bbd33ef4
riraq <riraq@mail.usf.edu>
2021-04-29 16:20:36 -0500

    updated put route for updating workout

--- public/api.js
+++ public/api.js
@@ -29,7 +29,7 @@ const API = {
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
     });
-    console.log(res)
+
     const json = await res.json();
 
     return json;
--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -19,12 +19,21 @@ router.post('/workouts', async (req, res) => {
   }
 });
 
-router.put('/workouts/:id', async (req, res) => {
-  try {
-    res.status(200).json(userData);
-    } catch (err) {
-    res.status(400).json(err);
-  }
+router.put('/workouts/:id', async ({ params, body }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: { 
+        exercises: body 
+      }
+    }
+  )
+  .then(dbUser => {
+    res.json(dbUser);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
